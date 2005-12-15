68b5e68dbf349283f4ffb9772b58d7a492f2bf61
jnweiger <jnweiger>
2005-12-15 16:11:20 +0000

    take care of symlinks. cvs prefers to replace them by inplace copies. grrr.

--- mktar.pl
+++ mktar.pl
@@ -1,7 +1,6 @@
 #! /usr/bin/perl -w
 # 
 # create a tar ball snapshot from cvs repository.
-# oh, more bash than perl here....
 #
 # 2005-12-15, jw
 #
@@ -10,6 +9,21 @@ use Data::Dumper;
 
 my $srcdir = 'src';
 my $version_file = "$srcdir/patchlevel.h";
+my %symlinks = 
+(
+  'FAQ' => 'doc/FAQ',
+  'doc/install.sh' => '../install.sh'
+);
+
+for my $l (keys %symlinks)
+  {
+    my $ll = "$srcdir/$l";
+    next if -l $ll;
+    print "replacing $ll with a symlink to $symlinks{$l}\n";
+    unlink $ll;
+    symlink $symlinks{$l}, $ll;
+  }
+
 my %version;
 open IN, "<", $version_file or die "unable to read $version_file: $!";
 while (defined(my $line = <IN>))
@@ -24,5 +38,7 @@ mkdir $tmpdir or die "cannot mkdir $tmpdir: $!";
 system "cp -a $srcdir $tmpdir" and die "'cp -a $srcdir $tmpdir' failed: $!";
 rename "$tmpdir/$srcdir", "$tmpdir/screen-$version" or die "rename to screen-$version failed: $!";
 
-system "tar zcvf - -C $tmpdir > screen-$version.tar.gz screen-$version"
+system "tar zcf - -C $tmpdir > screen-$version.tar.gz screen-$version";
 system "rm -rf $tmpdir";
+
+print "screen-$version.tar.gz written.\n";
