86ca3370a6579a52b841ac16783f69a3e077fda9
jnweiger <jnweiger>
2005-12-19 20:20:47 +0000

    bug #15002 overview: screen 4.0.2 fails to compile successfully on aix 5.3 box fixed.

--- src/misc.c
+++ src/misc.c
@@ -613,7 +613,7 @@ char *value;
    */
 # endif /* NEEDSETENV */
 #else /* USESETENV */
-# if defined(linux) || defined(__convex__) || (BSD >= 199103)
+# if defined(linux) || defined(__convex__) || (BSD >= 199103) || defined(_AIX)
   setenv(var, value, 1);
 # else
   setenv(var, value);
