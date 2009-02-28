5c2192b2789765a00356b41279ea71d940fab8c5
Sadrul Habib Chowdhury <sadrul@users.sourceforge.net>
2009-02-28 01:14:09 -0500

    Change the default renditions.

--- src/ansi.c
+++ src/ansi.c
@@ -78,7 +78,7 @@ struct mchar mchar_null;
 struct mchar mchar_blank = {' ' /* , 0, 0, ... */};
 struct mchar mchar_so    = {' ', A_SO /* , 0, 0, ... */};
 
-int renditions[NUM_RENDS] = {6750175 /* =B dd */, 6750205 /* =u dd */};
+int renditions[NUM_RENDS] = {65529 /* =ub */, 65531 /* =b */};
 
 /* keep string_t and string_t_string in sync! */
 static char *string_t_string[] = 
