e6dc30e903717af7c826d2fba2fb499d3acbb768
Benjamin Porter <bporter@instructure.com>
2014-08-25 13:04:37 -0600

    Apple Apple patches for building on OSX

--- src/screen.c
+++ src/screen.c
@@ -109,6 +109,11 @@
 
 #include "logfile.h"	/* islogfile, logfflush */
 
+#ifdef __APPLE__
+#include <vproc.h>
+#include <vproc_priv.h>
+#endif
+
 #ifdef DEBUG
 FILE *dfp;
 #endif
@@ -1046,6 +1051,16 @@ char **av;
 	Panic(0, "No $SCREENDIR with multi screens, please.");
 #endif
     }
+#ifdef __APPLE__
+    else if (!multi && real_uid == eff_uid) {
+      static char DarwinSockDir[PATH_MAX];
+      if (confstr(_CS_DARWIN_USER_TEMP_DIR, DarwinSockDir, sizeof(DarwinSockDir))) {
+	strlcat(DarwinSockDir, ".screen", sizeof(DarwinSockDir));
+	SockDir = DarwinSockDir;
+      }
+    }
+#endif	/* __APPLE__ */
+
 #ifdef MULTIUSER
   if (multiattach)
     {
@@ -1313,6 +1328,11 @@ char **av;
 #endif
   freopen("/dev/null", "w", stderr);
   debug("-- screen.back debug started\n");
+
+#ifdef __APPLE__
+	if (_vprocmgr_detach_from_console(0) != NULL)
+		errx(1, "can't detach from console");
+#endif
 
   /* 
    * This guarantees that the session owner is listed, even when we
